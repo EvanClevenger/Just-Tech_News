ff8ae5ee479f949df1717ddc62041ab49c1678b0
EvanClevenger <102931271+EvanClevenger@users.noreply.github.com>
2022-08-15 21:18:22 -0500

    added login credentials

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 // import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
-require('dotenv').config();
+require('dotenv').config(); // need this
 // create connection to our database, pass in your MySQL information for username and password
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { 
   host: 'localhost',          //^^ this executes the info from .env
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,7 +4,9 @@ const { User } = require('../../models');
 // GET /api/users
 router.get('/', (req, res) => {
     //access our User modle and run .findALL() method
-    User.findAll() //findAll lets us query all of the users from the user table in the database, equivalent to SELECT * FROM users;
+    User.findAll({
+        attributes : { exclude : ['password']} //doesnt return password when using a GET request
+    }) //findAll lets us query all of the users from the user table in the database, equivalent to SELECT * FROM users;
         .then (dbUserData => res.json (dbUserData))
         .catch(err => {
             console.log(err);
@@ -14,7 +16,8 @@ router.get('/', (req, res) => {
 
 // GET /api/users/1
 router.get('/:id', (req, res) => {
-    User.findOne({ //findOne means we only want one peice of data back. also passing an argument in.  
+    User.findOne({ //findOne means we only want one peice of data back. also passing an argument in. 
+        attributes: { exclude: ['password'] }, 
         where: { //Where option used to indicate we want to find a user where its id value equals whatever req.params:id is equal to SELECT * FROM users WHERE id = 1
             id: req.params.id
         }
@@ -47,13 +50,36 @@ router.post('/', (req, res) => {
       });
     });
 
+
+    //login route
+    router.post ('/login', (req, res) =>{
+        User.findOne({ //looks for a user with a specific emial
+            where: {
+              email: req.body.email
+            }
+          }).then(dbUserData => {
+            if (!dbUserData) {
+             res.status(400).json({ message: 'No user with that email address!' });
+              return;
+           }
+           const validPassword = dbUserData.checkPassword(req.body.password);
+           if (!validPassword) {
+            res.status(400).json({message: "Incorrect password, please try again!"})
+            return;
+           }
+        
+            res.json({ user: dbUserData, message : "You're now loggin in!!" });
+        });
+    });
+
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
   // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
 
-  User.update(req.body, { //update method combines the paramaters for creating data and looking up data.
+  User.update(req.body, {  //update method combines the paramaters for creating data and looking up data.
+   individualHooks: true, //we must add this option to make beforeUpdate for the hook
     where:{ //req.body provides the new data we want to use in the update and req.params:id to indicate where exactly we want that new data to be used.
         id: req.params.id
     }
