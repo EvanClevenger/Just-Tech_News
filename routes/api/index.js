def81a298efc16e6a153dbf73a1ca563786835a3
EvanClevenger <102931271+EvanClevenger@users.noreply.github.com>
2022-08-16 11:06:30 -0500

    issues in post-routes or somewhere else?

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,9 +3,12 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes')
 
 router.use('/users', userRoutes);
 //we didn't use the word users in anyof the user-routes? 
 //That's because in this file we take those routes and implement them to another router instance, prefixing them with the path /users at that time.
+router.use('/posts', postRoutes);
+
 
 module.exports = router;
