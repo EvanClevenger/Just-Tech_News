c490b64452f4e61b04ba0e913a7c1ff5eda6dfe6
EvanClevenger <102931271+EvanClevenger@users.noreply.github.com>
2022-08-16 12:34:55 -0500

    fixed issue and added CRUD methods to POST route

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,15 +1,15 @@
 const router = require('express').Router(); //need express to create routes
 const {Post, User} = require ('../../models');
 
+//returns all
 router.get('/', (req, res) => {
     Post.findAll({
       attributes: ['id', 'post_url', 'title', 'created_at'],
-      include: [
-        {
+      order:[['created_at', 'DESC']], //shows the newest posts first
+      include: [{
           model: User,
           attributes: ['username']
-        }
-      ]
+        }]
     })
       .then(dbPostData => res.json(dbPostData))
       .catch(err => {
@@ -18,6 +18,7 @@ router.get('/', (req, res) => {
       });
   });
 
+  //Get a single post
   router.get('/:id', (req, res) => {
     Post.findOne({
       where: {
@@ -44,6 +45,8 @@ router.get('/', (req, res) => {
       });
   });
 
+
+  //create a post
   router.post('/', (req, res) => {
     // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
     Post.create({
@@ -57,5 +60,49 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
       });
   });
-  
+
+  //updates a post
+  router.put('/:id', (req ,res )=>{
+    Post.update({
+      title: req.body.title
+    },
+    {
+      where:{
+        id: req.params.id
+      }
+    }
+    )
+    .then(dbPostData => {
+      if (!dbPostData){
+        res.status(404).json({ message : 'No post found with this id'});
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+  });
+
+// delete a post 
+router.delete('/:id', (req, res) => {
+  Post.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
