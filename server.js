9587b2507deff57446a44747185451cfe1a88a02
EvanClevenger <102931271+EvanClevenger@users.noreply.github.com>
2022-10-02 19:39:36 -0500

    added helpers

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const helpers= require('./utils/helpers')//implements helpers
+
 
 //for handlebars
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({helpers});
 
 
 
